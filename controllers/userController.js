95a80a8c36368e665d4a7590e7c714a06f91e50c
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-12-31 16:36:02 -0800

    finished up some routes and added todos in user routes for moving forward. some of them will have to be moved to thoughtRoutes but this is good for now

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,36 +1,67 @@
-const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models');
 
-const headcount = async () => {
-    const numOfUsers = await User.aggregate().count('userCount');
-    return numOfUsers;
-};
-
 module.exports = {
-    //Get all users
+    // Get all users
     async getUsers(req, res) {
         try {
-            const users = await User.find();
-
-            const userObj = {
-                users,
-                headcount: await headcount(),
-            };
-
-            res.json(userObj);
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json(error);
+          const users = await User.find();
+          res.json(users);
+        } catch (err) {
+          res.status(500).json(err);
         }
-    },
-    // get a single user
+      },
+    // Get a single user
     async getUserById(req, res) {
         try{
             const user = await User.findOne({
-                _id: req.params.//UserId
-            })
+                _id: req.params.userId
+            }).select('-__v');
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+            res.json(user);
         } catch (error){
-
+            res.status(500).json(err);
         }
-    }
+    },
+    // Create a new user
+    async createUser(req, res) {
+        try {
+          const user = await User.create(req.body);
+          res.json(user);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+    },
+    async updateUser(req, res) {
+        try {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if(!updatedUser){
+                return res.status(404).json({ message: 'No application with this id!' });
+            };
+            res.json(updatedUser);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json(error);
+        }
+    },
+    async deleteUser(req, res) {
+        try {
+          const user = await User.findOneAndDelete({ _id: req.params.userId });
+    
+          if (!user) {
+            return res.status(404).json({ message: 'No user with that ID' });
+          }
+    
+          await Thought.deleteMany({ _id: { $in: user.thoughts } });
+          res.json({ message: 'User and associated thoughts deleted!' })
+        } catch (err) {
+          res.status(500).json(err);
+        };
+      },
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,22 @@
 const router = require('express').Router;
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/:userId
+router.route('/:userId').get(getUserById).put(updateUser).deleteUser(deleteUser);
+
+// TODO: /api/:userId/friends/:friendId POST to add a new friend and DELETE to remove a friend
+
+// TODO: /api/thoughts GET to get all thoughts, GET to get a single thought by its _id, POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field), PUT to update a thought by its _id, DELETE to remove a thought by its _id
+
+// TODO: /api/thoughts/:thoughtId/reactions POST to create a reaction stored in a single thought's reactions array field, DELETE to pull and remove a reaction by the reaction's reactionId value
+
+module.exports = router;
