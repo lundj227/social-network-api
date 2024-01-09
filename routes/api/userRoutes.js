a12508c5e9a620569cdd3ecb35992055ad06a063
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2024-01-08 21:38:58 -0800

    debugged

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,14 +3,14 @@ const router = require('express').Router();
 const userController = require('../../controllers/userController');
  
 // User Routes
-router.get('/', userController.getAllUsers);  
+router.get('/', userController.getUsers);  
 router.post('/', userController.createUser); 
 router.get('/:userId', userController.getUserById); 
-router.put('/:userId', userController.updateUserById); 
-router.delete('/:userId', userController.deleteUserById);
+router.put('/:userId', userController.updateUser); 
+router.delete('/:userId', userController.deleteUser);
 
 // Friend Routes
-router.post('/:userId/friends/:friendId', userController.addFriend);
-router.delete('/:userId/friends/:friendId', userController.removeFriend);
+router.post('/:userId/friends/:friendId', userController.addNewFriend);
+router.delete('/:userId/friends/:friendId', userController.deleteFriend);
 
 module.exports = router;
